c39bed51aadad0f6779424c23c2209dbe4e8e78f
chaiyuzhe <785487784@qq.com>
2020-06-18 11:48:12 +0800

    [Fix] 修复部分页面展示

--- js/all.js
+++ js/all.js
@@ -3,7 +3,7 @@ $('.open').click(function() {
   var i = parseInt($(this).attr('i'))
   var close = $(this).attr('data-close')
   var open = $(this).attr('data-open')
-  if(!$('.nav_m-list').is(":animated")) {
+  if(!$('.nav_m-list').is(':animated')) {
     $('.nav_m-list').slideToggle()
     if(i % 2 == 0) {
       $(this).attr('src', close)
@@ -14,15 +14,58 @@ $('.open').click(function() {
   }
   return false
 })
+
+function ifScrollTop() {
+  if ($(window).scrollTop() >= 100){
+    $(".top-bg").fadeIn(200)
+    $('.nav').addClass('fixed')
+    $('.mobile-nav').addClass('fixed')
+    $('.nav.index').hide()
+    $('.nav.fix').show()
+  } else {
+    $(".top-bg").fadeOut(200)
+    $('.nav').removeClass('fixed')
+    $('.mobile-nav').addClass('fixed')
+    $('.nav.fix').hide()
+    $('.nav.index').show()
+  }
+}
+
+$(function(){
+  //当滚动条的位置处于距顶部100像素以下时，跳转链接出现，否则消失
+  $(function () { 
+    ifScrollTop()
+    $(window).scroll(function(){
+      ifScrollTop()
+    });
+
+      //当点击跳转链接后，回到页面顶部位置
+    $(".top-bg").click(function(){
+      if ($('html').scrollTop()) {
+        $('html').animate({ scrollTop: 0 }, 1000)
+        return false;
+      }
+      $('body').animate({ scrollTop: 0 }, 1000)
+      return false;            
+    });       
+  });    
+});
+
+$('.erm-bg').hover(function() {
+  $('.ewm-img').css('display', 'block')
+}, function() {
+  $('.ewm-img').css('display', 'none')
+})
+
 var closeFlag = false
 $(document).ready(function(){
-  $("#code-btn").click(function() {
+  $('#code-btn').click(function() {
     const phone = $('#telPhone').val()
     if (!isPhoneNo(phone)) {
-      $('#phone-error').css("display","block")
+      $('#phone-error').css('display', 'block')
       return false
     } else {
-      $('#phone-error').css("display","none")
+      $('#phone-error').css('display', 'none')
     }
     $.ajax({
       url:'https://www.yourlanqing.com/App/verifyCode/' + phone + '/3',
@@ -30,7 +73,7 @@ $(document).ready(function(){
       success: function (returnValue) {
         if (returnValue.code) {
           $('#code-error').html('验证码发送失败，还在有效期')
-          $('#code-error').css("display", "block")
+          $('#code-error').css('display', 'block')
           return
         }
         $('#code-btn').attr('disabled', true)
@@ -51,12 +94,12 @@ $(document).ready(function(){
       },
       error: function (returnValue) {
         $('#code-error').html('验证码获取失败,请刷新重试')
-        $('#code-error').css("display", "block")
+        $('#code-error').css('display', 'block')
       }
     })
   })
 
-  $("#submit-btn").click(function() {
+  $('#submit-btn').click(function() {
     const getObj = {
       address: $('#address').val(),
       contactPerson: $('#contactPerson').val(),
@@ -66,34 +109,34 @@ $(document).ready(function(){
     }
 
     if (!getObj.address) {
-      $('#address-error').css("display","block")
+      $('#address-error').css('display','block')
       return false
     } else {
-      $('#address-error').css("display","none")
+      $('#address-error').css('display','none')
     }
     if (!getObj.contactPerson) {
-      $('#user-error').css("display","block")
+      $('#user-error').css('display','block')
       return false
     } else {
-      $('#user-error').css("display","none")
+      $('#user-error').css('display','none')
     }
     if (!getObj.phone) {
-      $('#phone-error').css("display","block")
+      $('#phone-error').css('display','block')
       return false
     } else {
-      $('#phone-error').css("display","none")
+      $('#phone-error').css('display','none')
     }
     if (!getObj.storeName) {
-      $('#company-error').css("display","block")
+      $('#company-error').css('display','block')
       return false
     } else {
-      $('#company-error').css("display","none")
+      $('#company-error').css('display','none')
     }
     if (!getObj.verifyCode) {
-      $('#code-error').css("display","block")
+      $('#code-error').css('display','block')
       return false
     } else {
-      $('#code-error').css("display","none")
+      $('#code-error').css('display','none')
     }
     $.ajax({
       url:'https://www.yourlanqing.com/Web/storeResident',
@@ -102,17 +145,17 @@ $(document).ready(function(){
       data: JSON.stringify(getObj),
       success: function (returnValue) {
         if (returnValue.code) {
-          if (closeFlag) $('#message-tip').css("transform", "translateY(-200%)")
+          if (closeFlag) $('#message-tip').css('transform', 'translateY(-200%)')
           messageTip(returnValue.message)
-          $('#message-tip').css("background", "#fef0f0")
-          $('#message-tip').css("color", " #f56c6c")
+          $('#message-tip').css('background', '#fef0f0')
+          $('#message-tip').css('color', ' #f56c6c')
           close()
           return
         }
-        if (closeFlag) $('#message-tip').css("transform", "translateY(-200%)")
+        if (closeFlag) $('#message-tip').css('transform', 'translateY(-200%)')
         messageTip('提交成功，我们稍后会与你联系！')
-        $('#message-tip').css("background", "#f0f9eb")
-        $('#message-tip').css("color", "#57c32a")
+        $('#message-tip').css('background', '#f0f9eb')
+        $('#message-tip').css('color', '#57c32a')
         close()
         $('#address').val() = ''
         $('#contactPerson').val() = ''
@@ -121,7 +164,7 @@ $(document).ready(function(){
         $('#verifyCode').val() = ''
       },
       error: function (returnValue) {
-        if (closeFlag) $('#message-tip').css("transform", "translateY(-200%)")
+        if (closeFlag) $('#message-tip').css('transform', 'translateY(-200%)')
         messageTip(returnValue.message)
         close()
       }
@@ -131,15 +174,15 @@ $(document).ready(function(){
 
 function messageTip(msg) {
   $('#message-tip').html(msg)
-  $('#message-tip').css("background", "#fef0f0")
-  $('#message-tip').css("color", " #f56c6c")
-  $('#message-tip').css("transform", "translateY(0%)")
+  $('#message-tip').css('background', '#fef0f0')
+  $('#message-tip').css('color', ' #f56c6c')
+  $('#message-tip').css('transform', 'translateY(0%)')
 }
 
 function close() {
   closeFlag = true
   setTimeout(function() {
-    $('#message-tip').css("transform", "translateY(-200%)")
+    $('#message-tip').css('transform', 'translateY(-200%)')
   }, 3000)
 }
 
